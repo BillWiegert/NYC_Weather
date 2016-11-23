168b23df0de1da564d32964058e717de61fd416e
Bill Wiegert <billsteh@gmail.com>
2016-11-23 02:15:08 -0500

    Update OpenWeatherMap API key

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ console.log("Hello from the JavaScript console!");
 //   this.$h1 = $el.find("h1")
 // }
 $.ajax({
-  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=488c9179f2e875737d42014405c354f3",
   type: "GET",
   success(stuff) {
     doThingsWithStuff(stuff);
